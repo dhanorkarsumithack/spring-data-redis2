68757e45fa38da23d01754edf9d7b10caba216f7
sumitd <sumit.dhanorkar@neml.in>
2023-08-25 11:39:38 +0530

    redis-stream-consumer added

--- redis-stream-consumer/src/main/java/com/jskool/config/RedisConfig.java
+++ redis-stream-consumer/src/main/java/com/jskool/config/RedisConfig.java
@@ -1,2 +1,77 @@
-package com.jskool.config;public class RedisConfig {
+package com.jskool.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.stream.Consumer;
+import org.springframework.data.redis.connection.stream.ObjectRecord;
+import org.springframework.data.redis.connection.stream.ReadOffset;
+import org.springframework.data.redis.connection.stream.StreamOffset;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.stream.StreamListener;
+import org.springframework.data.redis.stream.StreamMessageListenerContainer;
+import org.springframework.data.redis.stream.Subscription;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.time.Duration;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    private final StreamListener<String, ObjectRecord<String , String>> streamListener;
+
+
+
+    @Bean
+    public LettuceConnectionFactory connectionFactory(){
+        RedisProperties properties=properties();
+        RedisStandaloneConfiguration configuration=new RedisStandaloneConfiguration();
+        configuration.setHostName(properties.getHost());
+        configuration.setPort(properties.getPort());
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    @Bean
+    public RedisTemplate<String ,Object> template(){
+        RedisTemplate<String, Object> template=new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+        template.afterPropertiesSet();
+        return  template;
+    }
+
+    @Bean
+    public RedisProperties properties(){
+        return new RedisProperties();
+    }
+
+    @Bean
+    public Subscription subscription() throws UnknownHostException {
+        var options = StreamMessageListenerContainer
+                .StreamMessageListenerContainerOptions
+                .builder()
+                .pollTimeout(Duration.ofSeconds(1))
+                .targetType(String.class)
+                .build();
+
+        var listnerCotainer = StreamMessageListenerContainer
+                .create(connectionFactory(),options);
+
+        var subscription = listnerCotainer.receiveAutoAck(
+                Consumer.from("sumitpub",InetAddress.getLocalHost().getHostName()),
+                 StreamOffset.create("sumitpub", ReadOffset.lastConsumed()),
+                streamListener
+                );
+        listnerCotainer.start();
+        return subscription;
+    }
 }
--- redis-stream-publisher/src/main/java/com/jskool/controller/PublisherController.java
+++ redis-stream-publisher/src/main/java/com/jskool/controller/PublisherController.java
@@ -1,2 +1,23 @@
-package com.jskool.controller;public class PublisherController {
+package com.jskool.controller;
+
+import com.jskool.publisher.Publisher;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PublisherController {
+
+    @Autowired
+    private final Publisher publisher;
+
+    @RequestMapping(value = "/publisher", method = RequestMethod.POST)
+    public String streamPublisher(@RequestBody String message){
+        publisher.streamPublisher(message);
+        return "publisher";
+    }
 }
