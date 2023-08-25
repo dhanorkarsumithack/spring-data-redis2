db7856193ff5b67632fcb9f540c4ebcfa507c179
sumitd <sumit.dhanorkar@neml.in>
2023-08-25 13:15:14 +0530

    redis pubsub with redis stream

---
+++ redis-stream-producer/src/main/java/com/example/redisstreampublisher/controller/ProducerController.java
@@ -0,0 +1,33 @@
+package com.example.redisstreampublisher.controller;
+
+import com.example.redisstreampublisher.producer.Producer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProducerController {
+
+    private static Logger log = LoggerFactory.getLogger(Producer.class);
+
+    @Autowired
+    private Producer producer;
+
+    @RequestMapping(value = "/publisher",method = RequestMethod.POST)
+    public ResponseEntity<Void> publish(@RequestBody String message) {
+
+        try {
+            log.info("publishing >>" + message);
+            producer.publishEvent(message.toString());
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+    }
+}
