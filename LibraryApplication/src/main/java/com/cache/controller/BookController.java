61156098ef350e094fb028dcab359862b9c61aed
Sumit Dhanorkar <91867665+dhanorkarsumithack@users.noreply.github.com>
2023-11-23 17:12:41 +0530

    Update BookController.java
    updated code

--- LibraryApplication/src/main/java/com/cache/controller/BookController.java
+++ LibraryApplication/src/main/java/com/cache/controller/BookController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//this is api of book
 @RestController
 @RequestMapping("/api/book")
 public class BookController {
